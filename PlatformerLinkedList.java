88d06f52adc1c345ed9b60bf30e2142a52fc1b12
Muza Khaja <muzamil.uddin@gmail.com>
2023-11-03 04:54:51 -0400

    Update PlatformerLinkedList.java

--- PlatformerLinkedList.java
+++ PlatformerLinkedList.java
@@ -2,36 +2,33 @@ import java.util.LinkedList;
 
 public class Platformer {
     private LinkedList<Integer> platform = new LinkedList<>();
-    private int currentPosition;
+    private int currentIndex;
 
     public Platformer(int n, int position) {
         for (int index = 0; index < n; index++) {
             this.platform.add(index);
         }
-        this.currentPosition = this.platform.get(position);
+        this.currentIndex = position;
     }
 
     public void jumpLeft() {
-        int oldIndex = this.platform.indexOf(currentPosition);
-        int newIndex = oldIndex - 2;
-        this.currentPosition = this.platform.get(newIndex);
+        int oldIndex = this.currentIndex;
+        this.currentIndex = oldIndex - 2;
         this.platform.remove(oldIndex);
     }
 
     public void jumpRight() {
-        int oldIndex = this.platform.indexOf(currentPosition);
-        int newIndex = oldIndex + 2;
-        this.currentPosition = this.platform.get(newIndex);
+        int oldIndex = this.currentIndex;
+        this.currentIndex = oldIndex + 1;
         this.platform.remove(oldIndex);
     }
 
     public int position() {
-        return this.currentPosition;
+        return this.platform.get(this.currentIndex);
     }
 
     public static void main(String[] args) {
         Platformer platformer = new Platformer(6, 3);
-        System.out.println(platformer.position()); // should print 3
 
         platformer.jumpLeft();
         System.out.println(platformer.position()); // should print 1
